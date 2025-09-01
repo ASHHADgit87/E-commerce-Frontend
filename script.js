38ae33387538b2e1095e044567da673e77887275
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2025-09-01 17:57:34 +0500

    Formatted Full code

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-const bar = document.getElementById('bar');
-const navbar = document.getElementById('navbar');
-const close = document.getElementById('close');
+const bar = document.getElementById("bar");
+const navbar = document.getElementById("navbar");
+const close = document.getElementById("close");
 
-if (bar)
-{
-    bar.addEventListener('click', () => {
-        navbar.classList.add('active');
-    }
-        
-    )
+if (bar) {
+  bar.addEventListener("click", () => {
+    navbar.classList.add("active");
+  });
 }
 
-
-if (close)
-{
-    close.addEventListener('click', () => {
-        navbar.classList.remove('active');
-    }
-        
-    )
+if (close) {
+  close.addEventListener("click", () => {
+    navbar.classList.remove("active");
+  });
 }
